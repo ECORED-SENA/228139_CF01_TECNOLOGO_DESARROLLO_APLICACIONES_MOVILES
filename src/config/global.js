2bf99c1e71d02ae61a6daa8d22a3aaf39da32301
Andrea Botello <andrea.botellor@misena.edu.co>
2023-08-16 10:32:12 -0500

    Entrega 1

--- src/config/global.js
+++ src/config/global.js
@@ -1,19 +1,10 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
+    componenteFormativo: 'La fase de elicitación de requisitos',
+    descripcionCurso:
+      'La recolección de datos se refiere al uso de una gran diversidad de técnicas y herramientas que pueden ser utilizadas por el analista para desarrollar los sistemas de información, que pueden ser la entrevista, la encuesta, el cuestionario, la observación, las sesiones en grupo, la visita a instalaciones, entre otros. Cada técnica de recolección de información posee diferentes instrumentos o herramientas para ser llevadas a cabo con profesionalismo y confiabilidad.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
-    imagenesDecorativasBanner: [
-      {
-        clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
-      },
-      {
-        clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
-      },
-    ],
   },
   menuPrincipal: {
     menu: [
@@ -31,28 +22,30 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Elicitación de requisitos',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Planeación',
             hash: 't_1_1',
           },
+          {
+            numero: '1.2',
+            titulo: 'Técnicas e instrumentos para elicitar requisitos',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Herramientas para captura de requisitos',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Herramientas de modelado',
+            hash: 't_1_4',
+          },
         ],
-      },
-
-      {
-        nombreRuta: 'tema2',
-        numero: '2',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Titulo de primer nivel',
-        desarrolloContenidos: true,
       },
     ],
     subMenu: [
@@ -102,32 +95,157 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: '1.1 Planeación',
+      referencia:
+        'todopmp.com. (s.f.). <em>Guía PMBOK 6 – 49 procesos, entradas, herramientas y salidas</em>.',
+      tipo: 'Página web',
+      link: 'https://todopmp.com/cards/',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: '1.1 Planeación',
+      referencia:
+        'EDAP – Project Business School. (2016). <em>MOOC PMP 302 Identificar Interesados</em> (video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=aUkTxgaajBo',
+    },
+    {
+      tema: '1.1 Planeación',
+      referencia:
+        'Virtual Training Lteam. (2016). <em>Partes Interesadas Stakeholders</em> (video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=9AtaIAZEu0c',
+    },
+    {
+      tema: '1.1 Planeación',
+      referencia:
+        'Calle, M. (2020). <em>Análisis de Interesados Mariz Poder Interés - PMI</em> (video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=hDZ0uu0H1wc',
+    },
+    {
+      tema: '1.2 Técnicas e instrumentos para elicitar requisitos',
+      referencia:
+        'Jibaro X. (2019). <em>Tipos de Pregundas en una encuesta</em> (video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=mwnQuUi9014',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Ciclo de vida <em>software</em>',
+      significado:
+        'se refiere a la aplicación de metodologías para el desarrollo del sistema <em>software</em> (AECC, 1986).',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Método',
+      significado:
+        'indica cómo construir técnicamente el <em>software</em>. Se incluyen técnicas de modelado y otras técnicas descriptivas.',
+    },
+    {
+      termino: 'Metodología',
+      significado: 'colección de métodos para resolver un tipo de problemas.',
+    },
+    {
+      termino: 'Ágil',
+      significado:
+        'comprende un conjunto de tareas o acciones que se utilizan para producir y mantener productos, así como para lograr los objetivos del proceso. La actividad incluye los procedimientos, estándares, políticas y objetivos para crear y modificar un conjunto de productos de trabajo.',
+    },
+    {
+      termino: 'Requerimiento',
+      significado:
+        'el requerimiento se refiere a la petición que se hace de algo que se solicita.',
+    },
+    {
+      termino: 'Requisito',
+      significado:
+        'es la condición que debe cumplir algo, en general el requisito cumple con lo que se requiere con el requerimiento.',
+    },
+    {
+      termino: '<em>Stakeholders</em>',
+      significado:
+        'individuo u organización que comparte, reclama o le interesa un sistema o le compete una característica que satisface sus necesidades y expectativas (ISO 29148).',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Baar, B. (2006). Using Stakeholder Analysis in Software Project Management. Universidad de Amsterdam.',
+    },
+    {
+      referencia:
+        'Braude, J. (2003). Ingeniería de software, una perspectiva orientada a objetos. Alfaomega.',
+    },
+    {
+      referencia:
+        'Cohen, L. (2011). Métodos de investigación educativa. La Muralla.',
+    },
+    {
+      referencia:
+        'Cohn, M. (2004). User Stories Applied for Agile Software Development. Pearson Education, Inc.',
+    },
+    {
+      referencia:
+        'Cox, K., Niazi, M., y Verner, J. (2009). Empirical study of Sommerville and Sawyer’s requirements engineering practices. IET Software, 3(5), 339.',
+      link: 'https://doi.org/10.1049/iet-sen.2008.0076',
+    },
+    {
+      referencia:
+        'Curso de interacción persona-ordenador. (2021). Storyboarding',
+      link: 'https://mpiua.invid.udl.cat/storyboarding',
+    },
+    {
+      referencia:
+        'Denscombe, M. (2010). The Good Research Guide. McGraw-Hill Education.',
+    },
+    {
+      referencia:
+        'Dornyei, Z. (2010). Questionnaires in Second Language Research: Construction, Administration, and Processing. Routledge.',
+    },
+    {
+      referencia:
+        'Durán, A., Bernárdez, B., Ruiz, A. y Toro, M. (1999). A Requirements Elicitatio Approach Based in Templates and Patterns.',
+      link:
+        'https://www.researchgate.net/publication/2890318_A_Requirements_Elicitation_Approach_Based_in_Templates_and_Patterns',
+    },
+    {
+      referencia:
+        'Gause, C., & Weinberg, G. M. (1989). Exploring Requirements: Quality Before Design. Dorset House.',
+    },
+    {
+      referencia:
+        'Granollers, T., Lorés, J., y Perdrix F. (2002). Prototipado. Capítulo 5: modelo de proceso de la ingeniería de la usabilidad y de la accesibilidad.',
+    },
+    {
+      referencia:
+        'Hernández, S., Fernández C., y Baptista L. (2006). Metodología de la investigación. McGraw Hill.',
+    },
+    {
+      referencia:
+        'Herrera J., Lizka J. (2003). Ingeniería de requerimientos, ingeniería de software.',
+      link: 'http://www.monografias.com/trabajos6/resof/resof.shtml',
+    },
+    {
+      referencia:
+        'La oficina de proyectos de informática (2012). PMOinformatica.com.',
+      link:
+        'http://www.pmoinformatica.com/2012/10/plantillas-scrum-historias-de-usuario.html',
+    },
+    {
+      referencia:
+        'Piattini M., Calvo-Manzano J., Cervera J., y Fernández, L. (2004). Análisis y diseño de aplicaciones informáticas de gestión. Una perspectiva de ingeniería de software. Alfaomega-Rama.',
+    },
+    {
+      referencia:
+        'Sommerville, I. (2011). Ingeniería de software. Pearson Educación.',
+    },
+    {
+      referencia:
+        'Ventura, M. T. (2002). La ingeniería de requerimientos como factor clave para el éxito de los proyectos de desarrollo de software.',
+      link: 'http://132.248.9.195/ppt2002/0307178/Index.html',
+    },
+    {
+      referencia:
+        'Wessinger, K., (2012) Project Stakeholder Management. Engineering Management Journal, 14(84), 19-24.',
     },
   ],
   creditos: [
@@ -143,7 +261,7 @@ export default {
           nombre: 'Rafael Neftalí Lizcano Reyes',
           cargo: 'Responsable de Línea de Producción',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
         },
       ],
     },
@@ -151,10 +269,56 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
+          nombre: 'Henry Eduardo Bastidas Paruma',
+          cargo: 'Experto temático',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro de teleinformática y producción industrial - Regional Cauca',
+        },
+        {
+          nombre: 'Jonathan Guerrero Astaiza',
+          cargo: 'Experto temático',
+          centro:
+            'Centro de teleinformática y producción industrial - Regional Cauca',
+        },
+        {
+          nombre: 'Peter Emerson Pinchao Solis',
+          cargo: 'Experto temático',
+          centro:
+            'Centro de teleinformática y producción industrial - Regional Cauca',
+        },
+        {
+          nombre: 'Zulema Yidney León Escobar',
+          cargo: 'Experta temática',
+          centro:
+            'Centro de teleinformática y producción industrial - Regional Cauca',
+        },
+        {
+          nombre: 'Oscar Absalón Guevara',
+          cargo: 'Diseñador instruccional',
+          centro: 'Centro de gestión industrial - Regional Bogotá',
+        },
+        {
+          nombre: 'Alix Cecilia Chinchilla Rueda',
+          cargo: 'Diseñadora instruccional',
+          centro: 'Centro de gestión industrial - Regional Bogotá',
+        },
+        {
+          nombre: 'José Gabriel Ortiz Abella',
+          cargo: 'Evaluador instruccional',
+          centro:
+            'Centro para la industria y la comunicación gráfica - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Julia Isabel Roberto',
+          cargo: 'Evaluadora instruccional',
+          centro:
+            'Centro para la industria y la comunicación gráfica - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Ana Catalina Córdoba Sus',
+          cargo: 'Metodólogo para formación virtual',
+          centro:
+            'Centro para la industria de la comunicación gráfica - Regional Distrito Capital',
         },
       ],
     },
@@ -162,28 +326,40 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Carmen Alicia Martínez Torres',
+          cargo: 'Animador y Productor Multimedia',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'Wilson Andrés Arenales Cáceres',
+          cargo: 'Storyboard e ilustración',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'Camilo Andrés Bolaño Rey',
+          cargo: 'Locución',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'Blanca Flor Tinoco Torres',
           cargo: 'Diseñador de Contenidos Digitales',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Desarrollador Fullstack',
+          nombre: 'Andrea Paola Botello De la Rosa',
+          cargo: 'Desarrollador Full-stack',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
         },
         {
-          nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
+          nombre: ' ',
+          cargo: 'Actividad didáctica',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
-        },
-        {
-          nombre: 'Nombre',
-          cargo: 'Actividad Didáctica',
-          centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
         },
       ],
     },
@@ -191,22 +367,22 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Zuleidy María Ruiz Torres',
-          cargo: 'Validador de Recursos Educativos Digitales',
-          centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
-        },
-        {
-          nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validador de Recursos Educativos Digitales',
-          centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
-        },
-        {
           nombre: 'Daniel Ricardo Mutis Gómez',
-          cargo: 'Evaluador para contenidos inclusivos y accesibles',
+          cargo: 'Evaluador para Contenidos Inclusivos y Accesibles',
           centro:
-            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'Zuleidy María Ruíz Torres',
+          cargo: 'Validador de Recursos Educativos Digitales',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        },
+        {
+          nombre: 'Luis Gabriel Urueta Álvarez',
+          cargo: 'Validador de Recursos Educativos Digitales',
+          centro:
+            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
         },
       ],
     },
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,6 +8,18 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    //figure.mb-4(data-aos="fade-down")
+      img(src='@/assets/curso/temas/img1.jpg' alt="")
+    
+    p(data-aos="fade") A través de este material, se tratan con detalle los pasos que se deben seguir en el proceso de recolección de datos, el uso de técnicas y los instrumentos para tal fin.
+    p.mb-4(data-aos="fade") Para comenzar, lo invitamos a ver el siguiente video:
+
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)        
+      figcaption Video. 
+
     
 </template>
 
